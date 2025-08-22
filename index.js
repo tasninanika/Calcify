c3f2c45829da901c657a86d0a8457e1f84946209
anikatasnin <jarintasnin09@gmail.com>
2025-08-22 09:31:15 +0600

    checking error

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const display = document.getElementById("display");
+let isError = false;
 
 function append(value) {
+  if (isError) {
+    display.value = "";
+    isError = false;
+  }
   display.value += value;
 }
 
