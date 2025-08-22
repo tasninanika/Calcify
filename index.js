c2f041313223fd97942ea7d23eb2150f4b97f87b
anikatasnin <jarintasnin09@gmail.com>
2025-08-22 08:36:35 +0600

    updated

---
+++ index.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
